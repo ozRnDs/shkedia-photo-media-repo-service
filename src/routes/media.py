5145174ca9488a5a9bb8b3b2ccd8a1613d347f17
oz <oz.rnds@gmail.com>
2023-11-25 20:37:19 +0000

    feat(src/routes/media): Create routes for uploading, downloading and deleting media from a reposervice

--- src/routes/media.py
+++ src/routes/media.py
@@ -1,23 +1,72 @@
-from fastapi import APIRouter, HTTPException, UploadFile
+from fastapi import APIRouter, HTTPException, UploadFile, Depends, Body
+from fastapi.responses import Response
 from pydantic import BaseModel
+from uuid import uuid4
+
+from repository.service import AWSRepoService
 
 class MediaClass(BaseModel):
-    media_name: str = None
-    media_content: str = None
+    media_id: str = str(uuid4())
+    media_content: bytes = None
+
+class SuccessResponse(BaseModel):
+    media_id: str = str(uuid4())
 
 
 router = APIRouter(tags=["Media"])
 
 
-@router.post("/")
-def post_media(media: UploadFile(...)):
-    media_name = media.file.filename()
-    return "test"
+class MediaServiceHandler:
+    def __init__(self, 
+                 repo_service: AWSRepoService,
+                 app_logging_service
+                 ):
+        self.repo_service = repo_service
+        self.logging_service = app_logging_service
+        if not self.repo_service.is_ready():
+            raise Exception("Can't initializes without repo_service")
+        self.router = self.__initialize_routes__()
 
-@router.get("/{media_id}",
-            response_model=str,
-            summary="Get media by id",
-            description="Get media from repo by id")
-def get_media(media_id: str):
-    return "This is media"
 
+    def __initialize_routes__(self):
+        router = APIRouter(tags=["media"])
+        router.add_api_route(path="/", 
+                             endpoint=self.post_media,
+                             methods=["post"],
+                             response_model=SuccessResponse)
+        router.add_api_route(path="/{media_id}", 
+                             endpoint=self.get_media,
+                             methods=["get"])
+        router.add_api_route(path="/{media_id}", endpoint=self.delete_media, methods=["delete"])
+        return router
+
+
+    def post_media(self, media: UploadFile):
+        temp_media_object = MediaClass(media_content=media.file.read())
+        try:
+            self.repo_service.upload(media_id=temp_media_object.media_id,
+                                     media_content=temp_media_object.media_content)
+            return SuccessResponse(media_id=temp_media_object.media_id)
+        except Exception as err:
+            #TODO: Log the full err
+            raise HTTPException(status_code=500,detail="Could not upload the image")
+
+    def get_media(self, media_id: str):
+        try:
+            media_byte = self.repo_service.download(media_id=media_id)
+            return Response(content=media_byte, media_type="application/octet-stream")
+        except FileNotFoundError as err:
+            #TODO: Log the full err
+            raise HTTPException(status_code=404,detail="Could not find media")
+        except Exception as err:
+            #TODO: Log the full err
+            raise HTTPException(status_code=500,detail="Can't reach images storage")
+
+    def delete_media(self, media_id: str):
+        try:
+            if self.repo_service.delete(media_id=media_id):
+                return SuccessResponse(media_id=media_id)
+            raise HTTPException("Media wasn't deleted")
+        except Exception as err:
+            #TODO: Log the full err
+            raise HTTPException("Can't delete the media")
